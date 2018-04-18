dfad071cb2a5d5381ebf4af43c32aa348b8edc9c
venturads <albert@venturads.com>
2018-04-18 15:53:12 -0700

    added css
    added css

--- getajax.js
+++ getajax.js
@@ -3,9 +3,14 @@ let getapi = "https://codepen.io/jobs.json";
 			getHtp.open("GET", getapi);
 			getHtp.onreadystatechange = function(){
 				if(getHtp.readyState == 4){
-					let getjson = JSON.parse(getHtp.responseText);
-					document.getElementById("display").innerHTML = "Job title: " + getjson.jobs[2].title + "<br>" + 
-					"url: " + getjson.jobs[0].url;
+					let getjson = JSON.parse(getHtp.responseText);				
+					for(let i=0;i<getjson.jobs.length;i++){
+						document.getElementById("display").innerHTML = "Job title: <a href=" + getjson.jobs[i].url + "><br>" + getjson.job[i].title + "</a>";
+						//+ "<br>Company: " + getjson.jobs[i].company_name + "<br>";
+						//getjson.jobs[i].address.city + ", " +
+						//getjson.jobs[i].address.state_prov;
+						//console.log(getjson.jobs.length);
+					}
 				}
-			};
+			}
 			getHtp.send();
