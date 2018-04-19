4b4e83bd1af89a4abcbefee4717be565afe34ea6
venturads <albert@venturads.com>
2018-04-19 12:50:07 -0700

    fixed loop
    fixed ajax for loop to show all query api  values.

--- getajax.js
+++ getajax.js
@@ -1,17 +1,18 @@
 let getapi = "https://codepen.io/jobs.json";
-for(let i=0;i<10;i++){
-		let getHtp = new XMLHttpRequest();
-		getHtp.open("GET", getapi);
-		getHtp.onreadystatechange = function(){
-			
-			if(getHtp.readyState == 4){
-			let getjson = JSON.parse(getHtp.responseText);				
-				document.getElementById("display").innerHTML = getjson.jobs[i].title + "<br>"
-				+ "<br>Company: " + getjson.jobs[i].company_name + "<br>";
-				console.log(getjson.jobs[i].company_name);
-				//getjson.jobs[i].address.city + ", " +
-				//getjson.jobs[i].address.state_prov;					
-				}
+let getHtp = new XMLHttpRequest();
+	getHtp.open("GET", getapi);
+	getHtp.onreadystatechange = function(){
+		if(getHtp.readyState == 4){
+			myList(this);
 			}
-		getHtp.send();
 		}
+	getHtp.send();
+		
+function myList(getHtp){
+	let table = "<br>";
+  let getjson = JSON.parse(getHtp.responseText);				
+	for(let i=0;i<getjson.jobs.length;i++){
+		table += "<a href='http://venturads.com'><br>" + getjson.jobs[i].title	+ "<br>Company: " + getjson.jobs[i].company_name + "<br>" +	getjson.jobs[i].address + ", " +	getjson.jobs[i].address + "</a><hr>";
+  }		
+	document.getElementById("display").innerHTML = table;
+}
