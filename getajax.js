311198e0f7488dec85b93245ace1c6b6c109c8d1
venturads <albert@venturads.com>
2018-04-18 18:10:24 -0700

    fix loop
    fix loop api

--- getajax.js
+++ getajax.js
@@ -1,16 +1,17 @@
 let getapi = "https://codepen.io/jobs.json";
-			const getHtp = new XMLHttpRequest();
-			getHtp.open("GET", getapi);
-			getHtp.onreadystatechange = function(){
-				if(getHtp.readyState == 4){
-					let getjson = JSON.parse(getHtp.responseText);				
-					for(let i=0;i<getjson.jobs.length;i++){
-						document.getElementById("display").innerHTML = "Job title: <a href=" + getjson.jobs[i].url + "><br>" + getjson.job[i].title + "</a>";
-						//+ "<br>Company: " + getjson.jobs[i].company_name + "<br>";
-						//getjson.jobs[i].address.city + ", " +
-						//getjson.jobs[i].address.state_prov;
-						//console.log(getjson.jobs.length);
-					}
+for(let i=0;i<10;i++){
+		let getHtp = new XMLHttpRequest();
+		getHtp.open("GET", getapi);
+		getHtp.onreadystatechange = function(){
+			
+			if(getHtp.readyState == 4){
+			let getjson = JSON.parse(getHtp.responseText);				
+				document.getElementById("display").innerHTML = getjson.jobs[i].title + "<br>"
+				+ "<br>Company: " + getjson.jobs[i].company_name + "<br>";
+				console.log(getjson.jobs[i].company_name);
+				//getjson.jobs[i].address.city + ", " +
+				//getjson.jobs[i].address.state_prov;					
 				}
 			}
-			getHtp.send();
+		getHtp.send();
+		}
